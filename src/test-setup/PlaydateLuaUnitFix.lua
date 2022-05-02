1b76f92ac4220bd44e92aa653cabe91ed854192e
neil-armstrong-fig <neil_armstrong_1991@hotmail.com>
2022-05-02 21:15:04 +0100

    Cleanup playdate luaunit fix file

--- src/test-setup/PlaydateLuaUnitFix.lua
+++ src/test-setup/PlaydateLuaUnitFix.lua
@@ -1,15 +1,9 @@
 -- stub out require
-
 function require()
 	-- do nothing
 end
 
-
-
-
-
 -- replace 'os' functionality
-
 os = {}
 
 function os.clock()
@@ -36,9 +30,7 @@ function nowAsString()
 	return nowString
 end
 
-
 -- replace 'io' functionality
-
 io = {}
 io.stdout = {}
 
@@ -58,12 +50,4 @@ function io.open(filename, readwrite)
 	local fd = playdate.file.open(filename, fileOpenAttr)
 	assert(fd)
 	return fd
-end
-
-function skipDueToIssue(issueURL)
-
-	local debugInfo = debug.getinfo(2)
-	local skipMessage = string.format("\n%s:%d Test SKIPPED due to issue %s", debugInfo.short_src, debugInfo.currentline, issueURL)
-	print(skipMessage)
-
 end
